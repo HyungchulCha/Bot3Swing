292f231dfddf24b16ddb7f30666984dce3cfbab1
ckgudcjf <ckgudcjf@gmail.com>
2023-04-30 23:01:15 +0900

    ing

---
+++ BotBacktest.py
@@ -0,0 +1,123 @@
+from Bot_3m import *
+from BotConfig import *
+import pandas as pd
+import numpy as np
+import os
+
+kosdaq = []
+B3 = Bot_3m()
+
+dir = os.getcwd()
+flist = os.listdir(dir + '/_back_3m')
+xlsx_list = np.array([x for x in flist if x.endswith('.xlsx')])
+
+ttl_code_array = []
+ttl_buy_array = []
+ttl_sel_array = []
+ttl_sucs_per_array = []
+ttl_fail_per_array = []
+ttl_prft_array = []
+
+for x in np.nditer(xlsx_list):
+    code = str(x).split('.')[0]
+
+    if not (code in kosdaq):
+        code_df = pd.read_excel(dir + '/_back_3m/' + str(x))
+        code_df = code_df[::-1]
+
+        temp_df = pd.DataFrame({'close': code_df['현재가'].abs().to_list()}).dropna()
+        ma5 = temp_df.rolling(5).mean()
+        ma20 = temp_df.rolling(20).mean()
+        ma60 = temp_df.rolling(60).mean()
+        df_l = len(temp_df.columns.to_list())
+        temp_df.insert(df_l, 'ma5', ma5)
+        temp_df.insert(df_l + 1, 'ma20', ma20)
+        temp_df.insert(df_l + 2, 'ma60', ma60)
+        temp_df['ma5p'] = temp_df['ma5'].shift()
+        temp_df['ma20p'] = temp_df['ma20'].shift()
+        temp_df['ma60p'] = temp_df['ma60'].shift()
+
+        # print(f'{code} ========================================================================')
+        # print(temp_df)
+
+        buy_p = 0
+        buy_c = 0
+        sucs_c = 0
+        fail_c = 0
+        item_buy_c = 0
+        item_sel_c = 0
+        _ror = 1
+
+        has_buy = False
+
+        for i, row in temp_df.iterrows():
+            
+            # if (row['ma5'] > row['ma5p']) and (row['ma5'] > row['ma20']) and (row['ma5'] >= row['ma60']) and (row['ma20'] < row['ma60']) and has_buy == False:
+            # if (row['ma5'] > row['ma5p']) and (row['ma5'] > row['ma20']) and (row['ma5'] >= row['ma60']) and (row['ma20'] < row['ma60']) and (row['ma5p'] <= row['ma20p']) and (row['ma5p'] < row['ma60p']) and (row['ma20p'] < row['ma60p']) and has_buy == False:
+            # if (row['ma5'] > row['ma5p']) and (row['ma5'] > row['ma20']) and (row['ma5'] <= row['ma60']) and (row['ma20'] < row['ma60']) and (row['ma5p'] <= row['ma20p']) and (row['ma5p'] <= row['ma60p']) and (row['ma20p'] < row['ma60p']) and has_buy == False:
+            if (row['ma5'] > row['ma5p'])\
+            and (row['ma20'] > row['ma20p'])\
+            and (row['ma60'] > row['ma60p'])\
+            and (row['ma5p'] < row['ma20p'])\
+            and (row['ma5'] >= row['ma20'])\
+            and (row['ma5'] > row['ma60'])\
+            and (row['ma20'] > row['ma60'])\
+            and has_buy == False\
+            :
+                buy_p = buy_p + row['close']
+                has_buy = True
+                buy_c += 1
+                item_buy_c += 1
+
+            if (row['ma5'] <= row['ma5p']) and (row['ma5'] > row['ma20']) and (row['ma5'] > row['ma60']) and (row['ma20'] > row['ma60']) and has_buy == True:
+                if buy_c > 0:
+                    if buy_p * 0.98 > row['close'] * buy_c:
+                        sel_close = row['close'] * buy_c
+                        has_buy = False
+                        _ror = B3.ror(buy_p, sel_close, _ror)
+                        if sel_close - buy_p > 0:
+                            sucs_c += 1
+                        else:
+                            fail_c += 1
+                        item_sel_c += 1
+                        buy_p = 0
+                        buy_c = 0
+            # 73 #?288
+            if (row['ma5'] < row['ma5p']) and (row['ma5'] <= row['ma20']) and (row['ma5'] > row['ma60']) and (row['ma20'] > row['ma60']) and has_buy == True:
+                if buy_c > 0:
+                    sel_close = row['close'] * buy_c
+                    has_buy = False
+                    _ror = B3.ror(buy_p, sel_close, _ror)
+                    if sel_close - buy_p > 0:
+                        sucs_c += 1
+                    else:
+                        fail_c += 1
+                    item_sel_c += 1
+                    buy_p = 0
+                    buy_c = 0
+
+        if sucs_c != 0:
+            sucs_per = round(((sucs_c * 100) / (sucs_c + fail_c)), 2)
+            fail_per = round((100 - sucs_per), 2)
+            prft_per = round(((_ror - 1) * 100), 2)
+        else:
+            sucs_per = 0
+            if item_buy_c > 0:
+                fail_per = round((100 - sucs_per), 2)
+                prft_per = round(((_ror - 1) * 100), 2)
+            else:
+                fail_per = 0
+                prft_per = 0
+
+        ttl_code_array.append(code)
+        ttl_buy_array.append(item_buy_c)
+        ttl_sel_array.append(item_sel_c)
+        ttl_sucs_per_array.append(sucs_per)
+        ttl_fail_per_array.append(fail_per)
+        ttl_prft_array.append(prft_per)
+
+        print('종목:{}, 매수: {}회, 매도: {}회, 성공률 : {}%, 실패율 : {}%, 누적수익률 : {}%'.format(code, item_buy_c, item_sel_c, sucs_per, fail_per, prft_per))
+
+prft_df = pd.DataFrame({'code': ttl_code_array, 'buy': ttl_buy_array, 'sell': ttl_sel_array, 'success': ttl_sucs_per_array, 'fail': ttl_fail_per_array, 'profit': ttl_prft_array})
+prft_df = prft_df.sort_values('profit', ascending=False)
+prft_df.to_excel(dir + '/_back_result/Bot3mBacktest' + datetime.datetime.now().strftime('%m%d%H%M%S') + '.xlsx')
---
+++ BotConfig.py
@@ -0,0 +1,27 @@
+import os
+DIRECTORY = os.getcwd()
+
+KI_ID = "ckgudcjf"
+KI_URL_PRACTICE = 'https://openapi.koreainvestment.com:9443'
+KI_ACCOUNT_PRACTICE = '19213040-01'
+KI_ACCOUNT_PRACTICE_C = '19213040'
+KI_APPKEY_PRACTICE = 'PSBbKwct4IS2abFaFknj5t5G1fesOvUbKovh'
+KI_APPSECRET_PRACTICE = 'V9QdcVXKpjz1vFk5LCA2G6aXrbSDk4bk/+cXckKOk0/x1NqumlNW1MB8mvCaAexNauUWTlTwVDbkelgZEeGMkWmfE3c5eYuqt9YWD+81u6k7Nrhbf6CSlkzlHES50qL5hUEDdLW6XlR1l7c0/ENrTqoU2RZI1LDmtxE/9cvAUfzczRx25Qs='
+
+KI_URL_IMITATION = 'https://openapivts.koreainvestment.com:29443'
+KI_ACCOUNT_IMITATION = '50081061-01'
+KI_ACCOUNT_IMITATION_C = '50081061'
+KI_APPKEY_IMITATION = 'PSgFaLpCMY9CLFFUCaguWyDVdnOwnfYNQSPs'
+KI_APPSECRET_IMITATION = 'FfxV9JO+iOSy+OiwyPl3Yh+UM7koPNcv0z6X6omA7w+0CCz7WSUtcMU+zz9ZL7P73YOGq8sVyr6UgII+R8lfczwTVcXeb0WQQUEB7/D1rz9TrH13vVjCVA8ciTBGi+QDj+bV4r5HJBJBZpgzRbCALs6D3XsC+4QXuwkT/leKBQTbROWqobU='
+
+LINE_URL = 'https://notify-api.line.me/api/notify'
+LINE_TOKEN = '48zl8RmuB0lZoPOoVmqowZzjsUE0P53JO7jfVFCyLwh'
+
+FILE_URL = DIRECTORY + '/_data'
+FILE_URL_BACK = DIRECTORY + '/_back_3m/'
+FILE_URL_QUANT_TOTAL = FILE_URL + '/QuantData.xlsx'
+FILE_URL_DATA_3M = FILE_URL + '/BotData_3m.xlsx'
+FILE_URL_DATA_BACK_3M = FILE_URL + '/BotData_3m_backup.xlsx'
+FILE_URL_QUANT_LIST_3M = FILE_URL + '/QuantDataList_3m.pickle'
+FILE_URL_QUANT_RANK_3M = FILE_URL + '/QuantDataRank_3m.xlsx'
+FILE_URL_QUANT_LAST_3M = FILE_URL + '/QuantLast_3m.pickle'
