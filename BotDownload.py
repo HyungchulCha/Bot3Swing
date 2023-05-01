42f69d6370a222e86730b8a5f2bdb51ece144372
ckgudcjf <ckgudcjf@gmail.com>
2023-05-01 21:23:53 +0900

    ing

--- BotDownload.py
+++ BotDownload.py
@@ -1,46 +1,11 @@
 from Bot3Swing import *
 from BotConfig import *
 from pykiwoom.kiwoom import *
-import pandas as pd
-import numpy as np
 import time
 import pickle
-import os
 
 B3 = Bot3Swing()
-kp = B3.bkk.fetch_kospi_symbols()
-kp = kp.loc[(kp['그룹코드'] == 'ST') 
-        & (kp['시가총액규모'] != 0) 
-        & (kp['시가총액'] != 0) 
-        & (kp['우선주'] == 0) 
-        & (kp['단기과열'] == 0) 
-        & (kp['락구분'] == 0)
-        & (kp['액면변경'] == 0) 
-        & (kp['증자구분'] == 0)
-        & (kp['ETP'] != 'Y') 
-        & (kp['SRI'] != 'Y') 
-        & (kp['ELW발행'] != 'Y') 
-        & (kp['KRX은행'] != 'Y') 
-        & (kp['KRX증권'] != 'Y')
-        & (kp['KRX섹터_보험'] != 'Y')
-        & (kp['SPAC'] != 'Y') 
-        & (kp['거래정지'] != 'Y') 
-        & (kp['정리매매'] != 'Y') 
-        & (kp['관리종목'] != 'Y') 
-        & (kp['시장경고'] != 'Y') 
-        & (kp['경고예고'] != 'Y') 
-        & (kp['불성실공시'] != 'Y') 
-        & (kp['우회상장'] != 'Y') 
-        & (kp['공매도과열'] != 'Y') 
-        & (kp['이상급등'] != 'Y') 
-        & (kp['회사신용한도초과'] != 'Y') 
-        & (kp['담보대출가능'] != 'Y') 
-        & (kp['대주가능'] != 'Y') 
-        & (kp['신용가능'] == 'Y')
-        & (kp['증거금비율'] != 100)
-        & (kp['전일거래량'] > 300000) 
-        ]
-B3.save_file(FILE_URL_QUANT_LIST_3M, kp['단축코드'].to_list())
+save_file(FILE_URL_QUANT_LAST_3M, B3.bkk.filter_code_list())
 
 # 2 키움에서 5분봉 데이터 추출
 # /_back_5m 폴더에 xlsx 파일 저장
